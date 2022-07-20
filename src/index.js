0252484882f10faf88dfadfe6bcc2c1dab54e3a6
alyamba <logunionok@gmail.com>
2022-07-20 16:24:44 +0300

    fix: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(number) {
+    let reverseNumber = "";
+    number = Math.abs(number);
+    while (number != 0) {
+        reverseNumber = reverseNumber.concat(number % 10);
+        number = Math.trunc(number / 10);
+    }
+    return reverseNumber;
+};
